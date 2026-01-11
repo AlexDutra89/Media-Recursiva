829976a32f66c4e1a36cc22c6e698768d9f97e28
LeDutra89 <ledutra3@gmail.com>
2026-01-10 23:27:09 -0300

    commit 02

--- ProjetoMedia/src/Projeto/MediaRecursiva.java
+++ ProjetoMedia/src/Projeto/MediaRecursiva.java
@@ -3,24 +3,42 @@ package Projeto;
 import java.util.Scanner;
 
 public class MediaRecursiva {
-	
+
 	public static void main(String[] args) {
-		 Scanner sc = new Scanner(System.in);
-		 
-		 System.out.println("Digite a primeira nota: ");
-		 double nota1 = sc.nextDouble();
-		 
-		 System.out.println("Digite a segunda nota: ");
-		 double nota2 = sc.nextDouble();
-		 
-		 System.out.println("Digite a terceira nota: ");
-		 double nota3 = sc.nextDouble();
-		 
-		 double media = (nota1 + nota2 + nota3) / 3;
-		 
-		 System.out.printf("A média foi: %.2f", media);
-		 
-		 sc.close();
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Digite o número de notas: ");
+		int numeroNotas = sc.nextInt();
+
+		double[] notas = new double[numeroNotas];
+		double somaNotas = calcularSomaNotas(notas, 0, sc);
+		double media = calcularMedia(somaNotas, numeroNotas);
+		System.out.printf("Sua nota final é: %.2f\n", media);
+
+		sc.close();
 	}
 
+	public static double calcularSomaNotas(double[] notas, int i, Scanner sc) {
+		if (i == notas.length) {
+			return 0;
+		}
+		notas[i] = lerNotas(sc);
+		return notas[i] + calcularSomaNotas(notas, i + 1, sc);
+	}
+
+	public static double lerNotas(Scanner sc) {
+		System.out.println("Digite a nota: ");
+		double nota = sc.nextDouble();
+		if (nota >= 0 && nota <= 10) {
+			return nota;
+		} else {
+			System.out.println("Nota inválida!!!!!\n Digite uma nota de 0 à 10!\n");
+			return lerNotas(sc);
+		}
+	}
+
+	public static double calcularMedia(double somaNotas, int numeroNotas) {
+		return somaNotas / numeroNotas;
+
+	}
 }
